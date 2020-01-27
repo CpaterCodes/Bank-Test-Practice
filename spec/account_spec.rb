5a788f4a401309f68f9e7bab47c4b233f8bc8fb2
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-01-27 11:55:38 +0000

    Tests: Set up SimpleCov and skeleton of bank account spec

---
+++ spec/account_spec.rb
@@ -0,0 +1,17 @@
+describe "Account" do
+  before :each do
+    account = Account.new
+  end
+
+  it "Should show a balance" do
+
+  end
+
+  it "Should accept a deposit" do
+
+  end
+
+  it "Should allow a withdrawal" do
+
+  end
+end
